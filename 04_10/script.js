be23123968b7f625451bb0e06ad41386c27ad673
DANNY <qnv164@gmail.com>
2020-07-29 00:00:00 +0700

    initilize project

---
+++ 04_10/script.js
@@ -0,0 +1,26 @@
+var course = new Object();
+
+var course = {
+    title: "JavaScript Essential Training",
+    instructor: "Morten Rand-Hendriksen",
+    level: 1,
+    published: true,
+    views: 0,
+    updateViews: function() {
+        return ++course.views;
+    }
+}
+
+console.log(course);
+course.updateViews(); // update views
+console.log(course);
+
+var x1 = new Object();    // A new Object object
+var x2 = new String();    // A new String object
+var x3 = new Number();    // A new Number object
+var x4 = new Boolean();   // A new Boolean object
+var x5 = new Array();     // A new Array object
+var x6 = new RegExp();    // A new RegExp object
+var x7 = new Function();  // A new Function object
+var x8 = new Date();      // A new Date object
+// https://www.w3schools.com/js/js_object_constructors.asp
