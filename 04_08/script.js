656e40d846b0ee63170a301cb92792e0e660d62d
DANNY <qnv164@gmail.com>
2020-07-29 00:00:00 +0700

    initilize project

---
+++ 04_08/script.js
@@ -0,0 +1,11 @@
+function logScope(){
+  var localVar = 2;
+
+  // let localVar=2;
+  if(localVar){
+    let localVar = "I'm different!";
+  }
+
+    console.log("logScope localVar =", localVar);
+}
+logScope();
