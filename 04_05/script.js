28612656d7af6c7525c62f3b7444a7c924a1be0a
DANNY <qnv164@gmail.com>
2020-12-25 23:14:31 +0700

    console.log(fractionResult);

--- 04_05/script.js
+++ 04_05/script.js
@@ -9,6 +9,14 @@ var secondFraction = 5/7;
 
 var fractionResult = findBiggestFraction(firstFraction,secondFraction);
 
-console.log("First fraction result: ", firstFraction);
-console.log("Second fraction result: ", secondFraction);
-console.log("Fraction " + fractionResult[0] + " with a value of " + fractionResult[1] + " is the biggest!");
+// console.log(fractionResult); // return result array so need to use console log
+/*
+  (2) ["firstFraction", 0.75]
+  0: "firstFraction"
+  1: 0.75
+  length: 2
+*/
+
+// console.log("First fraction result: ", firstFraction);
+// console.log("Second fraction result: ", secondFraction);
+// console.log("Fraction " + fractionResult[0] + " with a value of " + fractionResult[1] + " is the biggest!");
