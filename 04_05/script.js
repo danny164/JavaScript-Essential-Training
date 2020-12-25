d6663f192848de0d4a9e472ca5184366e2257f63
Thach <siriuspower360@gmail.com>
2020-12-26 00:50:03 +0700

    ...

--- 04_05/script.js
+++ 04_05/script.js
@@ -1,5 +1,6 @@
 function findBiggestFraction(a,b) {
     var result;
+    // cái này nè
     a>b ? result = ["firstFraction", a] : result = ["secondFraction", b];
     return result;
 }
@@ -8,7 +9,8 @@ var firstFraction = 3/4;
 var secondFraction = 5/7;
 
 var fractionResult = findBiggestFraction(firstFraction,secondFraction);
-
+console.log(firstFraction);
 console.log("First fraction result: ", firstFraction);
 console.log("Second fraction result: ", secondFraction);
 console.log("Fraction " + fractionResult[0] + " with a value of " + fractionResult[1] + " is the biggest!");
+// mảng của cái
