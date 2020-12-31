6e2a5d9cf74de9b25a32d77664f36a74518cb176
DANNY <qnv164@gmail.com>
2020-12-31 15:07:12 +0700

    slice(start, end)

--- 10_04/JS/script.js
+++ 10_04/JS/script.js
@@ -2,7 +2,8 @@ const IMAGES = document.querySelectorAll("img");
 
 for (let i = 0; i<IMAGES.length; i++) {
     let imgSrc = IMAGES[i].getAttribute("src");
-    imgSrc = imgSrc.slice(0,-8);
+    // images/testimonials/earth-800.jpg
+    imgSrc = imgSrc.slice(0, -8); // images/testimonials/earth
     console.log(imgSrc);
 
     let type = IMAGES[i].getAttribute("data-type");
